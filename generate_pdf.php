f8530009a9826b219c68d4fe79200e20b14d7c55
Ryan Losh <ryan@opticaleast.losh.lan>
2024-09-19 12:12:23 -0500

    Sync changes.

--- generate_pdf.php
+++ generate_pdf.php
@@ -1,1 +1,25 @@
-generate_pdf.php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    </head>
+    <body>
+        <?php 
+            $pdfChosen = $_POST["pdfChosen"];
+            $capConsent = $_POST["capConsent"];
+            $rxAck = $_POST["rxAck"];
+        ?>
+        <div class="container">
+            <div class="jumbotron">
+                <img class="rounded float-right" src="/images/lol-logo.png" alt="Logo">
+                <h1>Signatures for Rx's</h1>
+                <p>Bull$|-|it rule from tyrants!</p>
+            </div>
+            File:  <?php echo "$pdfChosen"; ?><br>
+            Signature capture consent: <?php echo "$capConsent"; ?><br>
+            Rx Receipt Acknowledgement:<?php echo "$rxAck"; ?><br>
+        </div>
+    </body>
+</html>
--- get_signature.php
+++ get_signature.php
@@ -17,7 +17,7 @@
                 $pdfChosen = $_POST["in_pdf"];
                 echo "[$pdfChosen]<br>";
             ?>
-            <iframe src="<?php echo "$pdfChosen"; ?>" width="100%" height="270"></iframe>
+            <iframe src="<?php echo "$pdfChosen"; ?>" width="100%" height="330"></iframe>
             <p>To comply with 16 CFR Part 456 Ophthalmic Practice Rule (eyeglass rule) June, 2024; and
                 to comply with 16 CFR Part 315 Contact Lens Rule July, 2004 (rules);
                 Losh Optometry LLC is required to have the patient sign a copy of their prescription at the conclusion
@@ -28,12 +28,11 @@
             <input type="hidden" name="mode" value="gen_pdf">
             <input type="hidden" name="pdfChosen" value="<?php echo "$pdfChosen"; ?>">
             <ul>
-                <li><input type="checkbox" name="cap_consent"> I consent for my signature to be electronically captured and affixed
+                <li><input type="checkbox" name="capConsent"> I consent for my signature to be electronically captured and affixed
                  to the copy of my prescription shown here.
-                <li><input type="checkbox" name="ack"> I acknowledge my prescription was issued to me at the conclusion of my examination.
+                <li><input type="checkbox" name="rxAck"> I acknowledge my prescription was issued to me at the conclusion of my examination.
             </ul>
             <hr>
-            Insert Signature Pad Here...
             <hr>
             <input type="submit" value="Apply Signature">
             </form>
