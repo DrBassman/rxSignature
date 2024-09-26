ccac8f7fa0489705826e585fc3b15bd0b9ab6d77
DrBassman <ryan@ryanlosh.com>
2024-09-25 22:43:13 -0500

    Move from Losh Optometry logo to generic logo.

--- page_header.php
+++ page_header.php
@@ -1,7 +1,7 @@
 <?php require __DIR__ . "/configfile.php"; ?>
 <div class="container">
     <div class="jumbotron">
-        <img class="rounded float-right" src="images/logo.svg" width="120" alt="Logo">
+        <img class="rounded float-right" src="images/logo.svg" width="12.5%" alt="Logo">
         <h1><?php echo "$app_name"; ?></h1>
         <p><?php echo "$app_tag"; ?></p>
     </div>
